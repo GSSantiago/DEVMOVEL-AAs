ffd0b695a4c08dff3f01974f810b78ecb8a8caed
Guilherme Santiago <guilhermes.santiago@hotmail.com>
2026-04-01 18:41:08 -0300

    Criação da viewmodel do placar

---
+++ PC02/app/src/main/java/com/example/placarbasquete/PlacarVIewModel.java
@@ -0,0 +1,32 @@
+package com.example.placarbasquete;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+
+public class PlacarVIewModel extends ViewModel{
+    private final MutableLiveData<Integer> pontuacaoEsquerdo = new MutableLiveData<>(0);
+    private final MutableLiveData<Integer> pontuacaoDireito = new MutableLiveData<>(0);
+
+    public LiveData<Integer> getPontuacaoEsquerdo() {
+        return pontuacaoEsquerdo;
+    }
+
+    public LiveData<Integer> getPontuacaoDireito() {
+        return pontuacaoDireito;
+    }
+
+    public void addPoints(String time, int pontuacao) {
+        if("esquerdo".equals(time)) {
+            int lastPontuacao = pontuacaoEsquerdo.getValue() != null ? pontuacaoEsquerdo.getValue() : 0;
+            int newValue = lastPontuacao + pontuacao;
+            pontuacaoEsquerdo.setValue(newValue);
+        }
+        else {
+            int lastPontuacao = pontuacaoDireito.getValue() != null ? pontuacaoDireito.getValue() : 0;
+            int newValue = lastPontuacao+ pontuacao;
+            pontuacaoDireito.setValue(newValue);
+        }
+     }
+}
